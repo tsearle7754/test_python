6bafd2f66f0b65938da3f13551144cf182d14a25
Thomas Mendez <tmendez7524@floridapoly.edu>
2025-11-12 19:18:14 +0000

    Implemented Computer choice

--- game.py
+++ game.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import random
 
 list1 = ["rock", "paper", "scissors"]
 
 print(f"Computer chose: {random.choice(list1)}")
-=======
-print("Welcome to Rock-Paper-Scissors!")
-
-# Player input
-choice = input("Choose rock, paper, or scissors: ")
-print(f"You chose: {choice}")
->>>>>>> e466558d5fc55c464584fa4eeb4345751076d8ba
