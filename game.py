abbbfe6935cac20fb478267dd79bc3ae484e9b64
Thomas Mendez <tmendez7524@floridapoly.edu>
2025-11-12 19:20:18 +0000

    roll back my commits because i forgot to screenshot the conflict screen

--- game.py
+++ game.py
@@ -1,10 +1,6 @@
-import random
 
-list1 = ["rock", "paper", "scissors"]
-print("Welcome to Rock-Paper-Scissors!")
+
 
 # Player input
 choice = input("Choose rock, paper, or scissors: ")
 print(f"You chose: {choice}")
-
-print(f"Computer chose: {random.choice(list1)}")
